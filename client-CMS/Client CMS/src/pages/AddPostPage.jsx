5b587926762115002a733ba482b57b0016ac0c26
Edwardo Samosir <edwardosamosir@gmail.com>
2023-05-07 14:39:07 +0700

    Feat: Add Edit Post and Add Post Page

--- client-CMS/Client CMS/src/pages/AddPostPage.jsx
+++ client-CMS/Client CMS/src/pages/AddPostPage.jsx
@@ -1,7 +1,76 @@
+import React, { useEffect, useState } from "react";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Link } from "react-router-dom";
+
 export default function AddPostPage() {
 
-    return (
-      <div>Ini Halaman AddPostPage</div>
-    )
-  }
-  
+  return (
+    <Container className="w-50" style={{marginTop:"100px"}}>
+      <h1 style={{color:'rgba(59,7,11,255)', marginTop: "120px", fontSize:"35px"}}>Formulir Informasi Kegiatan RT X</h1>
+      <Form style={{borderColor:'rgba(59,7,11,255)', marginTop: "25px"}}>
+          <Form.Group >
+            <Form.Label style={{color:'rgba(59,7,11,255)' , marginTop: "5px"}}>Nama Kegiatan</Form.Label>
+            <Form.Control
+              name="nama"
+              type="text"
+              placeholder="Nama Kegiatan"
+              style={{color:'rgba(59,7,11,255)', border: "1px solid rgba(59,7,11,255)", marginTop: "5px"}}
+            />
+            <Form.Label style={{color:'rgba(59,7,11,255)', marginTop: "10px"}}>Deskripsi Kegiatan</Form.Label>
+            <Form.Control
+              name="deskripsi"
+              as="textarea"
+              rows={8}
+              style={{color:'rgba(59,7,11,255)', border: "1px solid rgba(59,7,11,255)", marginTop: "5px"}}
+            />
+            <Form.Label style={{color:'rgba(59,7,11,255)', marginTop: "10px"}}>Kategori Kegiatan</Form.Label>
+            <Form.Control
+              name="kategori"
+              type="text"
+              placeholder="Kategori Kegiatan"
+              style={{color:'rgba(59,7,11,255)', border: "1px solid rgba(59,7,11,255)", marginTop: "5px"}}
+            />
+            <Form.Label style={{color:'rgba(59,7,11,255)', marginTop: "10px"}}>Lokasi Kegiatan</Form.Label>
+            <Form.Control
+              name="lokasi"
+              type="text"
+              placeholder="Lokasi Kegiatan"
+              style={{color:'rgba(59,7,11,255)', border: "1px solid rgba(59,7,11,255)", marginTop: "5px"}}
+            />
+            <Form.Label style={{color:'rgba(59,7,11,255)', marginTop: "10px"}}>Biaya Iuran Kegiatan</Form.Label>
+            <Form.Control
+              name="biaya"
+              type="text"
+              placeholder="Biaya Iuran Kegiatan"
+              style={{color:'rgba(59,7,11,255)', border: "1px solid rgba(59,7,11,255)", marginTop: "5px"}}
+            />
+          </Form.Group>
+          <Link to={'/'}>
+          <Button
+            style={{
+              marginTop: "30px",
+              backgroundColor:'white', 
+              borderColor:'rgba(59,7,11,255)',
+              color:'rgba(59,7,11,255)',
+              marginRight:'10px',
+            }} 
+          >
+          Batalkan
+          </Button>
+        </Link>  
+        <Button 
+          style={{
+            marginTop: "30px",
+            backgroundColor:'rgba(59,7,11,255)', 
+            borderColor:'rgba(59,7,11,255)'
+          }} 
+          type="submit"
+        >
+          Tambahkan
+        </Button>
+      </Form>
+    </Container>
+  );
+}
